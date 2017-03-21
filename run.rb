f735b8370f3440778226d599646404fcbc4d0a69
Paco Sanchez <sanchezpaco@Pacos-MacBook-Pro.local>
2017-03-21 13:25:59 +0100

    Adds logic for post wercker step logs to SumoLogic

--- run.rb
+++ run.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/env ruby
 
-puts ENV['WERCKER_SUMOLOGIC_NOTIFIER_URL']
+# Obtain the run steps
+# curl -H 'Authroization: Bearer 3c1b3efd37a371c9a181aed868b6edfb04791585380b14e973aef036251d9137' https://app.wercker.com/api/v3/runs/58d102334b67450100fd8de3/steps
+# Iterate each step and extract the log, then post it to sumologic collector
+#
+
+require 'httparty'
+require 'json'
+
+token = ENV['WERCKER_TOKEN']
+run_endpoint = ENV['WERCKER_RUN_URL']
+headers = {"Authorization" => "Bearer #{token}"}
+sumologic_collector_url = ENV['SUMOLOGIC_COLLECTOR_URL']
+
+response = HTTParty.get("#{run_endpoint}/steps", headers: headers)
+response_json = JSON.parse(response.body)
+
+for step in response_json
+  log_url = step["logUrl"]
+  log_response = HTTParty.get(log_url, headers: headers)
+  log_data = log_response.body
+  step["logContent"] = log_data
+
+  sumologic_post_response = HTTParty.post(sumologic_collector_url,
+    :body => step.to_json,
+    :headers => { 'Content-Type' => 'application/json' } )
+
+end
